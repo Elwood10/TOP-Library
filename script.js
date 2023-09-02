b5683e06530a54c4925163a3a489f8928a05c42a
Roy Carter <roycartercpa@gmail.com>
2023-09-02 11:05:02 -0400

    set up basic form that adds book object to myLibrary array on submit, so the engine works

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked ? "I have read it." : "No, I haven't read it.";
+
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  
+  // Clear the form fields after adding the book
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").checked = false;
 }
+
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  console.log(myLibrary);
+});
+
