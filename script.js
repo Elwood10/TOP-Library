d8dd2ebbb586cb3ac6b37fe87b36782e074eba14
Roy Carter <roycartercpa@gmail.com>
2023-09-07 16:03:05 -0400

    Cleaned out a few lines at work

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 /* make array to hold the book entries*/
-const myLibrary = [];
+const myLibrary = []
 /* creates the book objects for the library*/
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
 }
 /* gets the event to run the add to myLibrary*/
-let addBook = document.getElementById("addBook")
-addBook.addEventListener("click", addIt())
-function addIt(){
-
-}
+//let addBook = document.getElementById("addBook")
+//addBook.addEventListener("click", addIt())
+//function addIt() {}
 /* gets the input from the form for the new book*/
-function addBookToLibrary() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked ? "I have read it." : "No, I haven't read it.";
+//THIS DIDN'T DO ANYTHING, DELETE IT
 
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  
-  // Clear the form fields after adding the book
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-  document.getElementById("read").checked = false;
+function addBookToLibrary() {
+	let title = document.getElementById("title").value
+	let author = document.getElementById("author").value
+	let pages = document.getElementById("pages").value
+	let read = document.getElementById("read").checked
+		? "I have read it."
+		: "No, I haven't read it."
+
+	let newBook = new Book(title, author, pages, read)
+	myLibrary.push(newBook)
+
+	// Clear the form fields after adding the book
+	document.getElementById("title").value = ""
+	document.getElementById("author").value = ""
+	document.getElementById("pages").value = ""
+	document.getElementById("read").checked = false
 }
 
-let submitButton = document.getElementById("submit");
+let submitButton = document.getElementById("submit")
 
 submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-  console.log(myLibrary);
-});
+	e.preventDefault()
+	addBookToLibrary()
+	console.log(myLibrary)
+})
 /* set up the modal pop up for adding books to the library*/
-let openForm = document.getElementById("addBook");
-let closeForm = document.getElementById("close-form");
+let openForm = document.getElementById("addBook")
+//let closeForm = document.getElementById("close-form")
 let getForm = document.getElementById("form-section")
+//THIS ISN'T DOING ANYTHING, DELETE IT
 
-openForm.addEventListener('click', () => {
-  getForm.showModal();
+openForm.addEventListener("click", () => {
+	getForm.showModal()
 })
