302deb6899f2e5f2975dc39901a780a66d246c1c
Roy Carter <roycartercpa@gmail.com>
2023-09-19 22:08:41 -0400

    reworked how the dom was populated for the books, need to find a way to refresh page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* make array to hold the book entries*/
 const myLibrary = []
+
 /* creates the book objects for the library*/
 function Book(title, author, pages, read) {
 	this.title = title
@@ -19,12 +20,15 @@ function addBookToLibrary() {
 	let newBook = new Book(title, author, pages, read)
 	myLibrary.push(newBook)
 
+  
+
 	// Clear the form fields after adding the book
 	document.getElementById("title").value = ""
 	document.getElementById("author").value = ""
 	document.getElementById("pages").value = ""
 	document.getElementById("read").checked = false
 }
+
 
 /* set up the modal pop up for adding books to the library*/
 let openForm = document.getElementById("addBook")
@@ -36,12 +40,16 @@ openForm.addEventListener("click", () => {
 let submitButton = document.getElementById("submit")
 submitButton.addEventListener("click", (e) => {
 	e.preventDefault()
+  
 	addBookToLibrary()
+
+
+
 
 	console.log(myLibrary)
 
-	let latestBook = myLibrary[myLibrary.length - 1]
-	console.log(latestBook)
+  myLibrary.forEach((Book) => {
+	
 
 	const bookList = document.getElementById("container2")
 	let card = document.createElement("div")
@@ -49,23 +57,39 @@ submitButton.addEventListener("click", (e) => {
 	bookList.appendChild(card)
 
 	let bookSpace = document.createElement("p")
-	let bookSpaceTitle = document.createTextNode(`title: ${latestBook.title}`)
+	let bookSpaceTitle = document.createTextNode(`title: ${Book.title}`)
 	bookSpace.appendChild(bookSpaceTitle)
 
 	let bookSpace2 = document.createElement("p")
-	let bookSpaceAuthor = document.createTextNode(`author: ${latestBook.author}`)
+	let bookSpaceAuthor = document.createTextNode(`author: ${Book.author}`)
 	bookSpace2.appendChild(bookSpaceAuthor)
 
 	let bookSpace3 = document.createElement("p")
-	let bookSpacePages = document.createTextNode(`pages: ${latestBook.pages}`)
+	let bookSpacePages = document.createTextNode(`pages: ${Book.pages}`)
 	bookSpace3.appendChild(bookSpacePages)
 
 	let bookSpace4 = document.createElement("p")
-	let bookSpaceRead = document.createTextNode(`Read: ${latestBook.read}`)
+	let bookSpaceRead = document.createTextNode(`Read: ${Book.read}`)
 	bookSpace4.appendChild(bookSpaceRead)
+
+  
+  
+  //set up remove from library button//
+  let bookSpace5 = document.createElement("button")
+	let bookSpaceRemove = document.createTextNode("Remove")
+bookSpace5.classList.add("removeBook")
+	bookSpace5.appendChild(bookSpaceRemove)
+
+  card.remove()
 
 	card.appendChild(bookSpace)
 	card.appendChild(bookSpace2)
 	card.appendChild(bookSpace3)
 	card.appendChild(bookSpace4)
+  card.appendChild(bookSpace5)
+  
+  console.log(myLibrary)
+  })
 })
+  
+  
