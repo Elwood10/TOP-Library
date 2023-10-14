a13ab57087f08821d15b835f0a1bcb7dcebdd75a
Roy Carter <roycartercpa@gmail.com>
2023-10-14 19:15:09 -0400

    Figured out how to clear page and refresh with each new addition by wiping out div that contains the info, used similar to deal with deleted books

--- script.js
+++ script.js
@@ -1,95 +1,121 @@
-/* make array to hold the book entries*/
-const myLibrary = []
+//set up array to store books
+const myLibrary = [];
+let card
+//initialize display for later function
 
-/* creates the book objects for the library*/
+let display;
+let bookList;
+
+//open the add book form
+let openForm = document.getElementById("addBook");
+let getForm = document.getElementById("form-section");
+openForm.addEventListener("click", () => {
+	getForm.showModal();
+})
+
+//creates book object
 function Book(title, author, pages, read) {
-	this.title = title
-	this.author = author
-	this.pages = pages
-	this.read = read
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
 }
-
+	
+    
+//gets form info to create new object for library array
 function addBookToLibrary() {
-	let title = document.getElementById("title").value
-	let author = document.getElementById("author").value
-	let pages = document.getElementById("pages").value
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let pages = document.getElementById("pages").value;
 	let read = document.getElementById("read").checked
 		? "I have read it."
-		: "No, I haven't read it."
+		: "No, I haven't read it.";
 
-	let newBook = new Book(title, author, pages, read)
-	myLibrary.push(newBook)
-
-  
+	let newBook = new Book(title, author, pages, read);
+	myLibrary.push(newBook);
 
 	// Clear the form fields after adding the book
-	document.getElementById("title").value = ""
-	document.getElementById("author").value = ""
-	document.getElementById("pages").value = ""
-	document.getElementById("read").checked = false
+	document.getElementById("title").value = "";
+	document.getElementById("author").value = "";
+	document.getElementById("pages").value = "";
+	document.getElementById("read").checked = false;
+}
+
+//on submit click pushes new book to array and displays book on webpage
+
+let submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	addBookToLibrary();
+
+	//Clears page of previous activity for refreshed info
+	document.getElementById("container2").innerHTML = "";
+    display();
+})
+
+//creates html elements for page to run on submit
+ display = function() {
+
+	myLibrary.forEach((Book) => {
+
+	const bookList = document.getElementById("container2");
+	let card = document.createElement("div");
+	card.id = "inventory";
+	card.classList.add("inventory")
+	
+	bookList.append(card);
+
+	let bookSpace = document.createElement("p");
+	let bookSpaceTitle = document.createTextNode(`title: ${Book.title}`);
+	bookSpace.append(bookSpaceTitle);
+
+	let bookSpace2 = document.createElement("p");
+	let bookSpaceAuthor = document.createTextNode(`author: ${Book.author}`);
+	bookSpace2.append(bookSpaceAuthor);
+
+	let bookSpace3 = document.createElement("p");
+	let bookSpacePages = document.createTextNode(`pages: ${Book.pages}`);
+	bookSpace3.append(bookSpacePages);
+
+	let bookSpace4 = document.createElement("p");
+	let bookSpaceRead = document.createTextNode(`Read: ${Book.read}`);
+	bookSpace4.append(bookSpaceRead);
+
+//set up remove from library button//
+let bookSpace5 = document.createElement("button");
+let bookSpaceRemove = document.createTextNode("Remove");
+bookSpace5.classList.add("removeBook");
+bookSpace5.id = "removeBook";
+bookSpace5.append(bookSpaceRemove);
+
+card.append(bookSpace);
+card.append(bookSpace2);
+card.append(bookSpace3);
+card.append(bookSpace4);
+card.append(bookSpace5);
+bookList.append(card)
+
+})
+//removes book from library array and reloads page with new booklist 
+const begone = document.getElementsByClassName("removeBook");
+const buttn = Array.from(begone)
+  buttn.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      const index = Array.from(buttn).indexOf(e.target);
+     
+      let deleteBook = index
+      myLibrary.splice(deleteBook,1)
+     console.log(myLibrary)
+
+	 //refreshes page after removal of book
+	 document.getElementById("container2").innerHTML = "";
+    display();
+
+    })
+  
+    })
+
 }
 
 
-/* set up the modal pop up for adding books to the library*/
-let openForm = document.getElementById("addBook")
-let getForm = document.getElementById("form-section")
-openForm.addEventListener("click", () => {
-	getForm.showModal()
-})
-//add books to html//
-let submitButton = document.getElementById("submit")
-submitButton.addEventListener("click", (e) => {
-	e.preventDefault()
-  
-	addBookToLibrary()
 
-
-
-
-	console.log(myLibrary)
-
-  myLibrary.forEach((Book) => {
-	
-
-	const bookList = document.getElementById("container2")
-	let card = document.createElement("div")
-	card.classList.add("inventory")
-	bookList.appendChild(card)
-
-	let bookSpace = document.createElement("p")
-	let bookSpaceTitle = document.createTextNode(`title: ${Book.title}`)
-	bookSpace.appendChild(bookSpaceTitle)
-
-	let bookSpace2 = document.createElement("p")
-	let bookSpaceAuthor = document.createTextNode(`author: ${Book.author}`)
-	bookSpace2.appendChild(bookSpaceAuthor)
-
-	let bookSpace3 = document.createElement("p")
-	let bookSpacePages = document.createTextNode(`pages: ${Book.pages}`)
-	bookSpace3.appendChild(bookSpacePages)
-
-	let bookSpace4 = document.createElement("p")
-	let bookSpaceRead = document.createTextNode(`Read: ${Book.read}`)
-	bookSpace4.appendChild(bookSpaceRead)
-
-  
-  
-  //set up remove from library button//
-  let bookSpace5 = document.createElement("button")
-	let bookSpaceRemove = document.createTextNode("Remove")
-bookSpace5.classList.add("removeBook")
-	bookSpace5.appendChild(bookSpaceRemove)
-
-  card.remove()
-
-	card.appendChild(bookSpace)
-	card.appendChild(bookSpace2)
-	card.appendChild(bookSpace3)
-	card.appendChild(bookSpace4)
-  card.appendChild(bookSpace5)
-  
-  console.log(myLibrary)
-  })
-})
-  
-  
